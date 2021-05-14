9380a28ba1e7fae1c24b22466dc4dd0a16047ac7
supervisor <d.yacenko@gmail.com>
2021-05-14 09:38:17 +0300

    Add VK auth app

---
+++ VKAuthApplication/app/src/main/java/ru/samsung/itschool/vkauthapplication/MainActivity.kt
@@ -0,0 +1,45 @@
+package ru.samsung.itschool.vkauthapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAccessToken
+import com.vk.api.sdk.auth.VKAuthCallback
+import com.vk.api.sdk.auth.VKScope
+import ru.samsung.itschool.vkauthapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btLogin.setOnClickListener({
+            VK.login(this, arrayListOf(VKScope.WALL, VKScope.PHOTOS))
+        })
+        binding.btLogoff.setOnClickListener({VK.logout()})
+    }
+
+     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val callback = object: VKAuthCallback {
+            override fun onLogin(token: VKAccessToken) {
+                // User passed authorization
+                binding.tvRes.text="Login success"
+            }
+
+            override fun onLoginFailed(errorCode: Int) {
+                // User didn't pass authorization
+                binding.tvRes.text="Login fail"
+            }
+        }
+        if (data == null || !VK.onActivityResult(requestCode, resultCode, data, callback)) {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+}
